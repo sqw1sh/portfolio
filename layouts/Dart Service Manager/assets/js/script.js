eab552ae761781b1784ff5ce892bb714eae3132e
sqw1sh <98525753+sqw1sh@users.noreply.github.com>
2023-11-29 14:01:41 +0300

    Add files via upload

---
+++ layouts/Dart Service Manager/assets/js/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.accordion-item-trigger').forEach((item) =>
+  item.addEventListener('click', () => {
+    const parent = item.parentNode;
+
+    if(parent.classList.contains('accordion-item-active')) {
+      parent.classList.remove('accordion-item-active');
+    } else {
+      document.querySelectorAll('.accordion-item').forEach((child) =>
+        child.classList.remove('accordion-item-active'));
+
+      parent.classList.add('accordion-item-active');
+    }
+  })
+ )
