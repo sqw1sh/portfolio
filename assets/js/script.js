ae3d3a121b652291123609f1b8306442029226a4
sqw1sh <98525753+sqw1sh@users.noreply.github.com>
2023-12-16 15:06:06 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-const headAccordeon = document.querySelector('.head-accordeon');
-const accordeon = document.querySelector('.accordeon');
+const urlStr = window.location.href;
+var url = new URL(urlStr);
+var paramValue = url.searchParams.get("section");
 
-headAccordeon.addEventListener('click', () => {
-    accordeon.classList.toggle('active');
-});
+let bodies = document.querySelectorAll(".card-body");
+let items = document.querySelectorAll(".card-foot-item");
+
+switch(paramValue) {
+    case "1":
+        switchActive(1, bodies, items);
+        break;
+    case "2":
+        switchActive(2, bodies, items);
+        break;
+}
+
+function switchActive(number, ...arg) {
+    for(let i=0; i<arg.length; i++) {
+        for(let j=0; j<arg[i].length; j++) {
+            arg[i][j].classList.remove("active");
+        }
+    }
+    arg[0][number-1].classList.add("active");
+    arg[1][number-1].classList.add("active");
+}
